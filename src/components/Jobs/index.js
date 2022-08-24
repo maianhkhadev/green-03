0bcf5c305762063b7cfc3ed9a3d7b97756c64bd2
MAY13\May13 <ntoan010599@gmail.com>
2022-08-24 19:29:10 +0700

    123

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -33,22 +33,52 @@ const Jobs = () => {
   }
 
   const onSubmit = () => {
-
+    if(formData.id){
+      createJob(formData).then(res => {
+        setList(res.data)
+       setFormData[DEFAULT_FORM_DATA]
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+    if(formData.id){
+      editJob(formData).then(res => {
+        setList(res.data)
+       setFormData[DEFAULT_FORM_DATA]
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
   }
 
   const onCreate = () => {
-    
+    setFormData(DEFAULT_FORM_DATA)
+
   }
 
   const onEdit = data => {
-
+      setFormData(data)
   }
 
   return (
     <div>
-      <header className='header row'>
-        <h4 className='col-7'>Tasks</h4>
-      </header>
+      {list.map((item) => {
+        return (
+          <div key={item.id}>
+            <div>{item.title}</div>
+            <div>{item.type}</div>
+            <div>{item.descriptor}</div>
+            <button>Edit</button>
+          </div>
+        )
+      })
+
+
+      }
+
+     
     </div>
   )
 }
