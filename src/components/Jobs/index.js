cb65cbf9ea35df2e9b3533729f6c85882a40adff
QuangQuang <lenhatquanggl2018@gmail.com>
2022-08-24 19:51:55 +0700

    Quang commit first

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,12 +1,45 @@
 import { useState, useEffect } from 'react'
 import './style.css'
-import { getJobs, createJob, editJob } from '../../apis/jobs'
 
-const DEFAULT_FORM_DATA = { title: '', type: '', descriptor }
+import { getJobs, createJob, editJob, deleteJob } from '../../apis/jobs'
+
+
+
+
+
+const DEFAULT_FORM_Data = { title: '', type: '', descriptor: '' }
+
+const validate = (list, data) => {
+  console.log(data)
+      if (data.name === '' || data.email === '') {
+          return false
+      }
+
+      // validate Create: kiểm tra trùng ở nút Create
+      if (!data.id) { 
+          const newItem = list.find((item) => {
+              return item.name === data.name || item.email === data.email
+          })
+          return newItem ? false : true
+      }
+
+
+      // validate nút Edit: kiểm tra trùng ở nút Edit
+      if (data.id) {
+          const newItem = list.find((item) => {
+              return item.id !== data.id && (item.name === data.name || item.email === data.email)
+          })
+          return newItem ? false : true
+      }
+}
+
+
 
 const Jobs = () => {
-  const [formData, setFormData] = useState(DEFAULT_FORM_DATA)
+  const [formData, setFormData] = useState(DEFAULT_FORM_Data)
   const [list, setList] = useState([])
+  const [error, setError] = useState (false)
+
 
   useEffect(() => {
     fetchData()
@@ -15,7 +48,7 @@ const Jobs = () => {
   const fetchData = () => {
     getJobs()
       .then(response => {
-        setList(response.data)
+        setList(response.formData)
       })
       .catch(error => {
         console.log(error)
@@ -33,15 +66,60 @@ const Jobs = () => {
   }
 
   const onSubmit = () => {
+    const isValidated = validate(list, formData)
+        console.log(isValidated)
+
+        if(!isValidated) {
+            setError(true)
+        }
+
+        if(isValidated) {
+        
+        if(!formData.id) { // nút Create
+            createJob(formData).then((res) => {
+                fetchData()
+                setFormData(DEFAULT_FORM_Data)
+            }).catch((error) => { console.log(error) })
+        
+        }
+        if(formData.id) { // nút Edit
+            editJob(formData.id, formData).then((res) => {
+                fetchData()
+                setFormData(DEFAULT_FORM_Data)
+            }).catch(error => { console.log(error) 
+            })
+           
+        }
+        const element = document.querySelector('#modal-form-user')
+        const modal = window.bootstrap.Modal.getOrCreateInstance(element);
+        modal.hide();
+        }
 
   }
 
   const onCreate = () => {
-    
+    setFormData(DEFAULT_FORM_Data)
+    const element = document.querySelector('#modal-form-user')
+    const modal = window.bootstrap.Modal.getOrCreateInstance(element);
+    modal.show();
   }
 
-  const onEdit = data => {
+  const onEdit = (formData) => {
+    setFormData(formData)
+    const element = document.querySelector('#modal-form-user')
+    const modal = window.bootstrap.Modal.getOrCreateInstance(element);
+    modal.show();
 
+  }
+  const onDelete = (id) => {
+    console.log(id)
+          deleteJob(id).then((res) => {
+            fetchData()
+        })
+        .catch((error) => { 
+            console.log(error) }
+            )
+            fetchData()
   }
 
   return (
